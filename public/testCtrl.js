ce3c335b94c01c6fc7fbc030db14a3e09fe22a6b
Henrik Nyh <henrik@nyh.se>
2014-05-30 17:20:10 +0200

    Can select buyer to bid as; dynamic colors

--- public/testCtrl.js
+++ public/testCtrl.js
@@ -1,28 +1,29 @@
-var testApp = angular.module("testApp", [ "ngAnimate", "timer" ]);
+var testApp = angular.module("testApp", [ "ngAnimate" ]);
 
 var bids = [
-  { amount: 400, buyer: 1, color: "powderblue", reserve_met: true, time: "2014-05-01 12:02" },
-  { amount: 350, buyer: 2, color: "chartreuse", reserve_met: false, time: "2014-05-01 12:01" },
-  { amount: 300, buyer: 1, color: "powderblue", reserve_met: false, time: "2014-05-01 12:00" },
+  { amount: 400, buyer: 1, reserve_met: true, time: "2014-05-01 12:02" },
+  { amount: 350, buyer: 2, reserve_met: false, time: "2014-05-01 12:01" },
+  { amount: 300, buyer: 1, reserve_met: false, time: "2014-05-01 12:00" },
 ];
 
 var initiallyTruncateAt = 5;
+var colors = [ "_", "powderblue", "chartreuse", "yellow", "pink", "green", "red" ];
 
 testApp.controller("testCtrl", function($scope) {
   $scope.truncateAt = initiallyTruncateAt;
 
+  $scope.colors = colors;
   $scope.bids = bids;
+  $scope.buyerId = 1;
   $scope.newBidAmount = 450;
   $scope.leadingBidAmount = 400;
-  $scope.endTime = new Date().getTime() + (1000 * 60 * 5);
 
   $scope.placeBid = function() {
     var amount = $scope.newBidAmount;
 
     var bid = {
       amount: amount,
-      buyer: 2,
-      color: "chartreuse",
+      buyer: $scope.buyerId,
       reserve_met: true,
       time: "2014-05-01 12:03",
     }
