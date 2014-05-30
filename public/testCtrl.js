648f677b9fd06ea816d429223c227cffa5ec912b
Henrik Nyh <henrik@nyh.se>
2014-05-30 12:39:01 +0200

    commented stuff out

--- public/testCtrl.js
+++ public/testCtrl.js
@@ -1,16 +1,40 @@
 var testApp = angular.module("testApp", []);
 
+/*
+
 var db = {
   1: { id: 1, name: "Pelle", snippet: "Allt om Pelle." },
   2: { id: 2, name: "Erik", snippet: "Erik är en fiskpinne." },
 };
 
+function dbValues() {
+  return Object.keys(db).map(function (key) { return db[key]; });
+};
+
 testApp.controller("testCtrl", function($scope) {
+
   console.log("nothing yet", $scope);
 
-  $scope.phones = Object.keys(db).map(function (key) { return db[key]; });
+  $scope.persons = dbValues();
 
-  $scope.viewPhone = function(id) {
-    $scope.phone = db[id];
+  $scope.viewPerson = function(id) {
+    $scope.person = db[id];
   };
+
+  $scope.addPerson = function() {
+    var id = parseInt(prompt("ID?", 3), 10);
+    var name = prompt("Name?", "Gob");
+
+    db[id] = {
+      id: id,
+      name: name,
+      snippet: "Allt om " + name + "."
+    };
+    $scope.persons = dbValues();
+
+    console.log("add: %o. %o", id, name);
+  };
+
 });
+
+*/
