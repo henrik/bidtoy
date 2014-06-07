a24acbe2929749eaf6aa5f9860060f1b53b3b9e0
Henrik Nyh <henrik@nyh.se>
2014-06-07 19:07:57 +0200

    A little more e2e testing

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -9,4 +9,11 @@ describe("Bid app", function() {
   it("changes the URL", function() {
     expect(ptor.getCurrentUrl()).toContain("location-changed-from-angular");
   });
+
+  it("lets you bid", function() {
+    var field = element(by.model("formBidAmount"));
+    field.sendKeys("12345");
+    element(by.css("button")).click();
+    // No expectation yet - was tricky to get that working.
+  });
 });
